01a034967bb49539b8b7b092b6a98b676b3eadc5
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-21 22:38:49 +0000

    Defined Movie#director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,5 +12,16 @@
 #  updated_at  :datetime         not null
 #  director_id :integer
 #
+
 class Movie < ApplicationRecord
+  def director
+
+    my_director_id = self.director_id
+
+    matching_directors = Director.where({ :id => my_director_id })
+
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
 end
